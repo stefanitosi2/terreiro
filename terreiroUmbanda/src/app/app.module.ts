efb8e082f2a6d61bfa7911ffc90dff8faead0068
Stefani Fernanda Pereira Tosi <stefanitosi2@hotmail.comgit config --global user.namegit config --global user.email stefanitosi2@hotmail.com>
2021-06-18 20:53:50 -0300

    site do terreiro

---
+++ terreiroUmbanda/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuTemplateComponent } from './components/menu-template/menu-template.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { OrixasPageComponent } from './components/orixas-page/orixas-page.component';
+import { AtendimentoPageComponent } from './components/atendimento-page/atendimento-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuTemplateComponent,
+    HomePageComponent,
+    OrixasPageComponent,
+    AtendimentoPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
