d03ca058bc997de3f56afc6d32364342f4304ce0
Stefani Fernanda Pereira Tosi <stefanitosi2@hotmail.comgit config --global user.namegit config --global user.email stefanitosi2@hotmail.com>
2021-06-18 22:50:12 -0300

    Dia 1 projeto

--- terreiroUmbanda/src/app/components/atendimento-page/atendimento-page.component.ts
+++ terreiroUmbanda/src/app/components/atendimento-page/atendimento-page.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./atendimento-page.component.css']
 })
 export class AtendimentoPageComponent implements OnInit {
-  public logoUrl='terreiro_n.jpg';
+  public logoUrl='https://www.iquilibrio.com/blog/wp-content/uploads/2017/01/ogum-1-1150x716.jpg?crop';
   public titulo='Casa De Caridade Caboclo Pena Branca e Velho Nagô';
   public descricao='É uma casa de caridade auxiliando, apoiando e direcionando espiritualmente, vivemos em prol daqueles que sempre necessitam dac caridade';
 
